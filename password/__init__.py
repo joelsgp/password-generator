819afc677cd887e4097d27e0d56c45eb55b8ad31
JMcB <joel.mcbride1@live.com>
2021-09-10 15:00:47 +0100

    Tidy formatting code

--- password/__init__.py
+++ password/__init__.py
@@ -19,7 +19,6 @@ with open(word_list_path) as words_file:
 ##print(words_list[0:50])
 
 
-# Function to add random words to string.
 def add_random_words(password, num_of_words=3, word_length_max=7):
     for i in range(num_of_words):
         while True:
@@ -32,7 +31,6 @@ def add_random_words(password, num_of_words=3, word_length_max=7):
 
     return password
 
-# Function to add random numerals to string.
 def add_random_numerals(password, digits=2):
     numerals = ""
 
@@ -43,20 +41,14 @@ def add_random_numerals(password, digits=2):
     return password
 
 
-# Actual password generation function, combines other two functions and prints
-def generate_password(pass_length_min=8, pass_length_max=12):
+##def generate_password(pass_length_min=8, pass_length_max=12):
+def generate_password():
     ##while len(password) > pass_length_max or len(password) < pass_length_min:
     password = ""
-
     password = add_random_words(password)
-
     password = add_random_numerals(password)
 
-    # Use password length to normalise placement of password length indicator.
-    # NOTE (vulnerability): this works, but not for arbitrary character counts
-    pass_length = len(password)
-    spacing = 30 - pass_length - len(str(pass_length))
-    print(password + " "*spacing + "-" + str(pass_length) + " characters")
+    print(f'{password:<30} - {len(password):<2} characters')
     pyperclip.copy(password)
 
     return password
